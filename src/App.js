ae7db52baa22bf811b16d19221d9e7c0a2f9fe32
Jordi <jordi.be.1992@gmail.com>
2023-09-07 01:40:50 +0200

    added slider and input

--- src/App.js
+++ src/App.js
@@ -10,23 +10,20 @@ export default function App() {
 }
 
 function Counter() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-
-  function handleMinusStep() {
-    setStep((step) => step - 1);
-  }
 
   function handleMinusCount() {
     setCount((count) => count - step);
   }
 
-  function handleAddStep() {
-    setStep((step) => step + 1);
-  }
-
   function handleAddCount() {
     setCount((count) => count + step);
+  }
+
+  function resetStates() {
+    setStep(1);
+    setCount(0);
   }
 
   const currentDate = new Date();
@@ -35,13 +32,25 @@ function Counter() {
   return (
     <>
       <div style={{ display: "flex", gap: 5, alignItems: "center", justifyContent: "center" }}>
-        <button onClick={handleMinusStep}>-</button>
-        <p>Step: {step}</p>
-        <button onClick={handleAddStep}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <p>{step}</p>
       </div>
       <div style={{ display: "flex", gap: 5, alignItems: "center", justifyContent: "center" }}>
         <button onClick={handleMinusCount}>-</button>
-        <p>Count: {count}</p>
+        <input
+          type="text"
+          min="0"
+          max="10"
+          placeholder="0"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <button onClick={handleAddCount}>+</button>
       </div>
       <div style={{ display: "flex", gap: 5, alignItems: "center", justifyContent: "center" }}>
@@ -49,6 +58,13 @@ function Counter() {
           {count === 0 ? `Today is` : count > 0 ? `${count} days from today is` : `${Math.abs(count)} days ago was`} {currentDate.toDateString()}
         </p>
       </div>
+      {step !== 1 || count !== 0 ? (
+        <div style={{ display: "flex", gap: 5, alignItems: "center", justifyContent: "center" }}>
+          <button onClick={resetStates}>Reset</button>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
